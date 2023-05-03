3043980a8f64b98e954ab51358493e5ddc9afbae
Miguel_Fr3 <rm99997@fiap.com.br>
2023-05-02 21:35:25 -0300

    salvando o dom

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-alert("Alô Mundo!");
+// var nome = "Luizinho";
+
+// if(nome != ""){
+//     let nome = "Nicolly";
+// }
+
+// console.log(nome);
+//Quando declaramos uma variável com var
+// o Javascript aplica um processo nestas que chamamos de HOISTING.
+//O HOISTING ou elevação, coloca todas as variáveis do tipo var no topo do programa.
+//Isso faz com que se existirem variáveis com mesmo nome, elas se sobrepõem.
+// let nr1 = 10;
+// let nr2 = 5;
+// //IF TERNÁRIO - DESCOBRINDO SE O NÚMERO É PAR OU IMPAR
+// let resultado = !(nr1 % nr2) ? "PAR" : "IMPAR";
+// console.log(resultado);
+
+
+//Recuperar um elemento do HTML com a função getElementById(parâmetro)
+const mn = document.getElementById("menu");
+console.log(mn.textContent);
