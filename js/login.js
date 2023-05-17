be520acf9eda0965fe82bc56fc7ad6fc9830ca64
Miguel_Fr3 <rm99997@fiap.com.br>
2023-05-16 22:54:16 -0300

    Arrumando o olho e login

--- js/login.js
+++ js/login.js
@@ -1,48 +1,78 @@
 "use strict";
 
-const usuario1 = {
-    nomeUsuario : "rita-lee",
-    senhaUsuario : "00000"
-}
-const usuario2 = {
-    nomeUsuario : "ptt01",
-    senhaUsuario : "12345"
-}
+//CRIANDO OBJETOS
+let listaDeUsuarios = [
 
-// console.log(usuario1.nomeUsuario);
-// console.log(usuario2.nomeUsuario);
-// usuario2.nomeUsuario = "Juquinha";
-// console.log(usuario2.nomeUsuario);
+    {
+        nomeCompleto : "José das Couves",
+        nomeUsuario : "jose",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "João Paulino",
+        nomeUsuario : "joao",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Maria Tomaite",
+        nomeUsuario : "maria",
+        senhaUsuario : "123456"
+    }
+    ,
+    {
+        nomeCompleto : "Paulo da Selva",
+        nomeUsuario : "paulo",
+        senhaUsuario : "123456"
+    }
+];
 
-let listaDeUsuarios = [];
-listaDeUsuarios.push(usuario1);
-listaDeUsuarios.push(usuario2);
+localStorage.setItem("listaUser",JSON.stringify(listaDeUsuarios)); 
 
 addEventListener("click", (evento)=>{
+     
+    let userInput = document.querySelector("#idUser"); 
+    let passInput = document.querySelector("#idPass"); 
 
-    let userInput = document.querySelector("#idUser");
-    let passInput = document.querySelector("#idPass");
+    
+    // const eyePass = document.querySelector(".fa-eye");
 
-    if(evento.target.id == "btnSubmit"){
+    if(evento.target.className == "fa fa-eye" || evento.target.className == "fa fa-eye-slash"){
 
+        evento.target.setAttribute("style","cursor:pointer")
+
+        if(passInput.getAttribute("type") == "password"){
+            passInput.setAttribute("type","text");
+            evento.target.setAttribute("class","fa fa-eye-slash")
+        }else{
+            evento.target.setAttribute("style","cursor:pointer")
+            evento.target.setAttribute("class","fa fa-eye")
+            passInput.setAttribute("type","password");     
+        }
+
+    }
+
+    let listaDeUsuariosRecuperada = JSON.parse(localStorage.getItem("ListaUser"))
+        if(evento.target.id == "btnSubmit"){
+        
         try{
-        listaDeUsuarios.forEach( (usuario)=>{
+                listaDeUsuariosRecuperada.forEach((usuario)=>{
 
-            if(userInput.value == usuario.nomeUsuario && passInput.value ==
-                usuario.senhaUsuario){
-                throw "USUÁRIO VALIDADO!";
-            }
-        });
+                    if(userInput.value == usuario.nomeUsuario && passInput.value == usuario.senhaUsuario){
+                        throw "USUÁRIO VALIDADO!";
+                    }
+                });
 
-        throw "USÁRIO OU SENHA INCORRETOS!";
+                throw "USUÁRIO OU SENHA INCORRETOS!";
 
     }catch(msg){
         if(msg == "USUÁRIO VALIDADO!"){
             console.log("USUÁRIO VALIDADO!")
         }else{
-            console.log("USÁRIO OU SENHA INCORRETOS!")
+            console.log("USUÁRIO OU SENHA INCORRETOS!");
         }
     }
 
-    };
+  }
 });
