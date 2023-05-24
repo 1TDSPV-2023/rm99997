36b46b8c76399dfee0e29764c3b5b1781f5006af
Miguel_Fr3 <rm99997@fiap.com.br>
2023-05-23 22:35:51 -0300

    finalizando o sucesso

--- js/sucesso.js
+++ js/sucesso.js
@@ -1,13 +1,20 @@
-
-
 if(localStorage.getItem("user-token") != null){
     const bemVindoEl = document.querySelector("#bemvindo-user");
+    const userName = document.querySelector("#username");
+    const userImageContainer = document.querySelector(".profile-container");
+
     const botaoLogout = document.querySelector("#btnSair");
 
     //RECUPERANDO O OBJETO USER-VALIDADO DO LOCAL-STORAGE
     const usuarioValidado = JSON.parse(localStorage.getItem("user-validado"));
 
     bemVindoEl.innerHTML = usuarioValidado.nomeCompleto;
+    userName.innerHTML = usuarioValidado.nomeUsuario;
+
+    const newUserImage = document.createElement("img");
+    newUserImage.src = usuarioValidado.avatarUsuario;
+
+    userImageContainer.appendChild(newUserImage)
 
     //REMOVENDO OS ITENS DO LOCAL-STORE
     botaoLogout.addEventListener("click", ()=>{
